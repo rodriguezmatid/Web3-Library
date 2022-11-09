e822e6fd56455aa191ff613a7d6c316a281d5eb9
Matias Rodriguez <matias.demian.r@gmail.com>
2022-11-08 23:39:37 -0300

    swap

--- uniswap.py
+++ uniswap.py
@@ -8,8 +8,8 @@ _dotenv.load_dotenv()
 
 ganache_url = "HTTP://127.0.0.1:7545"
 infura_url = os.environ["INFURA_URL"]
-web3 = Web3(Web3.HTTPProvider(infura_url))
-privateKey = os.environ['PRIVATE_KEY']
+web3 = Web3(Web3.HTTPProvider('https://eth-goerli.g.alchemy.com/v2/yDYcpM9PmK5vZgiZKxa7RUEno1GoCkAL'))
+privateKey = os.environ['GOERLI_PRIVATE_KEY']
 
 # Log
 if web3.isConnected() == True:
@@ -33,16 +33,19 @@ factoryAddress = web3.toChecksumAddress('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5a
 routerAddress = web3.toChecksumAddress('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
 wethAddress = web3.toChecksumAddress('0xDf032Bc4B9dC2782Bb09352007D4C57B75160B15')
 account = web3.toChecksumAddress('0x596FbF10a5129fa7ae38FD6135C0954d415ea7Bc')
+account_2 = web3.toChecksumAddress('0x5144C21864E24235E81F7e8c4a8d60663666eA94')
 
 # Contracts
 contractFactory = web3.eth.contract(address = factoryAddress, abi = abi_contract_factory)
 routerContract = web3.eth.contract(address = routerAddress, abi = abi_router)
-
-# Quantity of pools
-poolslength = contractFactory.functions.allPairsLength().call()
+poolsDeseado = web3.toChecksumAddress(contractFactory.functions.getPair('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6','0x07865c6E87B9F70255377e024ace6630C1Eaa37F').call())
+print(poolsDeseado)
+# # Quantity of pools
+poolsLength = contractFactory.functions.allPairsLength().call()
+print(poolsLength)
 counter = 0
 
-for pool in range(0, 1):
+for pool in range(550, 551):
   counter += 1
 
   walletPool = web3.toChecksumAddress(contractFactory.functions.allPairs(pool).call())
@@ -56,14 +59,14 @@ for pool in range(0, 1):
 
   tokenBalance = poolContract.functions.getReserves().call()
 
-  tokenRatio = tokenBalance[1]/tokenBalance[0]
+  #tokenRatio = tokenBalance[1]/tokenBalance[0]
 
   token1Symbol = contract1.functions.symbol().call()
   token2Symbol = contract2.functions.symbol().call()
 
   print("Pool number #" + f"{counter}")
   print(walletPool)
-  print("Token Ratio: " + f"{tokenRatio}")
+  #print("Token Ratio: " + f"{tokenRatio}")
   print()
 
   #### Token 1 ####
@@ -78,62 +81,79 @@ for pool in range(0, 1):
   print("# Tokens: " + f"{web3.fromWei(tokenBalance[1], 'ether')}")
   print()
 
-# # Get the nonce // prevents you of sending a tx twice
-nonce = web3.eth.getTransactionCount(account)
+#   # Get the nonce // prevents you of sending a tx twice
+# nonce = web3.eth.getTransactionCount(account)
 
-swap = routerContract.functions.swapExactTokensForTokens(
-          1, #uint amountIn
-          0, #uint amountOutMin
-          [token1Address, token2Address], #address[] calldata path
-          account, #address to
-          int(time.time()) + (60*1) #uint deadline
-          ).buildTransaction({
-            'nonce': nonce,
-            'from': account,
-            'value': web3.toWei(1, 'ether'),
-            'gas': 2000000,
-            'gasPrice': web3.toWei('50', 'gwei')
-          })
-
-addLiquidity = routerContract.functions.addLiquidity(
-          token1Address, # address tokenA,
-          token2Address, # address tokenB,
-          # uint amountADesired,
-          # uint amountBDesired,
-          # uint amountAMin,
-          # uint amountBMin,
-          # address to,
-          int(time.time()) + (60*1) # uint deadline
-          ).buildTransaction({
-            'nonce': nonce,
-            'from': account,
-            'value': web3.toWei(1, 'ether'),
-            'gas': 2000000,
-            'gasPrice': web3.toWei('50', 'gwei')
-          })
-
-removeLiquidity = routerContract.functions.removeLiquidity(
-          token1Address, # address tokenA,
-          token2Address, # address tokenB,
-          # uint liquidity,
-          # uint amountAMin,
-          # uint amountBMin,
-          # address to,
-          int(time.time()) + (60*1) # uint deadline
-          ).buildTransaction({
-            'nonce': nonce,
-            'from': account,
-            'value': web3.toWei(1, 'ether'),
-            'gas': 2000000,
-            'gasPrice': web3.toWei('50', 'gwei')
-          })
-
-
+  # swap = routerContract.functions.swapExactTokensForTokens(
+  #           1, #uint amountIn
+  #           0, #uint amountOutMin
+  #           [token1Address, token2Address], #address[] calldata path
+  #           account, #address to
+  #           int(time.time()) + (60*1) #uint deadline
+  #           ).buildTransaction({
+  #             'nonce': nonce,
+  #             'from': account,
+  #             'value': web3.toWei(0.1, 'ether'),
+  #             'gas': 2000000,
+  #             'gasPrice': web3.toWei('50', 'gwei')
+  #           })
+# swap = routerContract.functions.swapExactETHForTokens(
+#             1, #uint amountOutMin
+#             ['0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', token2Address], #address[] calldata path
+#             account, #address to
+#             int(time.time()) + (60*1) #uint deadline
+#             ).buildTransaction({
+#               'nonce': nonce,
+#               'from': account,
+#               'value': web3.toWei(0.1, 'ether'),
+#               'gas': 2000000,
+#               'gasPrice': web3.toWei('50', 'gwei')
+#             })
 # # Sign transaction
 # signed_tx = web3.eth.account.signTransaction(swap, privateKey)
-
 # # Send transaction
-# send_tx = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-
+# tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 # # Get transaction hash
-# print(web3.toHex(send_tx))
+# print(web3.toHex(tx_hash))
+
+# addLiquidity = routerContract.functions.addLiquidity(
+#           token1Address, # address tokenA,
+#           token2Address, # address tokenB,
+#           # uint amountADesired,
+#           # uint amountBDesired,
+#           # uint amountAMin,
+#           # uint amountBMin,
+#           account, # address to,
+#           int(time.time()) + (60*1) # uint deadline
+#           ).buildTransaction({
+#             'nonce': nonce,
+#             'from': account,
+#             'value': web3.toWei(1, 'ether'),
+#             'gas': 2000000,
+#             'gasPrice': web3.toWei('50', 'gwei')
+#           })
+
+# removeLiquidity = routerContract.functions.removeLiquidity(
+#           token1Address, # address tokenA,
+#           token2Address, # address tokenB,
+#           # uint liquidity,
+#           # uint amountAMin,
+#           # uint amountBMin,
+#           account, # address to,
+#           int(time.time()) + (60*1) # uint deadline
+#           ).buildTransaction({
+#             'nonce': nonce,
+#             'from': account,
+#             'value': web3.toWei(1, 'ether'),
+#             'gas': 2000000,
+#             'gasPrice': web3.toWei('50', 'gwei')
+#           })
+
+# # Build a transaction
+# tx = {
+#     'nonce': nonce,
+#     'to': account_2,
+#     'value': web3.toWei(0.1, 'ether'),
+#     'gas': 2000000,
+#     'gasPrice': web3.toWei('50', 'gwei')
+# }
