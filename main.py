018752391be5b991537637f56fb1035dae587a31
Matias Rodriguez <matias.demian.r@gmail.com>
2022-11-13 22:04:33 -0300

    changes

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import os as os
 
 _dotenv.load_dotenv()
 
-ganache_url = "HTTP://127.0.0.1:7545"
+# ##########################
+# ###### Mainnet ######
+# ##########################
 infura_url = os.environ["INFURA_URL"]
-web3 = Web3(Web3.HTTPProvider(ganache_url))
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+# ##########################
+# ###### Testnet ######
+# ##########################
+#web3 = Web3(Web3.HTTPProvider(os.environ['GOERLI_URL']))
+#privateKey = os.environ['GOERLI_PRIVATE_KEY']
 
 # Allows you to read information about the blockchain, like the blocks and the transactions, create transactions, and interact with smart contracts
 # Log
@@ -16,65 +24,74 @@ if web3.isConnected() == True:
 else:
     print('Not logged')
 
-# Number of the last block in the blockchain
+############################################################################################################################################
+#################################################################  Address #################################################################
+############################################################################################################################################
+
+address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+
+############################################################################################################################################
+#################################################################  ABI's ###################################################################
+############################################################################################################################################
+
+with open('./utils/contract_factory.json', 'r') as f:
+  abi_contract_factory = json.load(f)
+
+with open('./utils/pools.json', 'r') as f:
+  abi_pools = json.load(f)
+
+with open('./utils/router.json', 'r') as f:
+    abi_router = json.load(f)
+
+# # Number of the last block in the blockchain
 #print("Last block number")
 #print(web3.eth.blockNumber)
 #print()
 
-# Eth balance from a wallet
-wallet = '0xfcE92BDB5D1bBA58A844Cff9dff8A5e680670Ade'
-#balance = web3.eth.get_balance(wallet)
-#print("Wallet balance in ETH")
-#print(web3.fromWei(balance, 'ether'))
-#print()
+# # Eth balance from a wallet
+# wallet = '0xfcE92BDB5D1bBA58A844Cff9dff8A5e680670Ade'
+# balance = web3.eth.get_balance(wallet)
+# print("Wallet balance in ETH")
+# print(web3.fromWei(balance, 'ether'))
+# print()
 
-# information of last block
+# # information of last block
 # print(web3.eth.getBlock(web3.eth.blockNumber))
 
 ############################################################################################################################################
 #################################################################  Token analysis ##########################################################
 ############################################################################################################################################
-
-# The ABI is a json array that describes the functions on the smart contract
-#abi = json.loads('[{"inputs":[{"internalType":"uint256","name":"chainId_","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"src","type":"address"},{"indexed":true,"internalType":"address","name":"guy","type":"address"},{"indexed":false,"internalType":"uint256","name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":true,"inputs":[{"indexed":true,"internalType":"bytes4","name":"sig","type":"bytes4"},{"indexed":true,"internalType":"address","name":"usr","type":"address"},{"indexed":true,"internalType":"bytes32","name":"arg1","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"arg2","type":"bytes32"},{"indexed":false,"internalType":"bytes","name":"data","type":"bytes"}],"name":"LogNote","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"src","type":"address"},{"indexed":true,"internalType":"address","name":"dst","type":"address"},{"indexed":false,"internalType":"uint256","name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"guy","type":"address"}],"name":"deny","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"move","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"holder","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"bool","name":"allowed","type":"bool"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"pull","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"usr","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"push","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"guy","type":"address"}],"name":"rely","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"wards","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]')
-#address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-
-#contract = web3.eth.contract(address=address, abi=abi)
-#totalSupply = contract.functions.totalSupply().call() # call functions implies reading data from the blockchain
-
-#dai = contract.functions.balanceOf(wallet).call()
-#print(web3.fromWei(dai, 'ether'))
+contract = web3.eth.contract(address=address, abi=abi_pools)
+totalSupply = contract.functions.totalSupply().call() # call functions implies reading data from the blockchain
 
 # Protocol name
-#print("Protocol name: " f"{contract.functions.name().call()}")
+print("Protocol name: " f"{contract.functions.name().call()}")
 # Token
-#print("Token: " f"{contract.functions.symbol().call()}")
+print("Token: " f"{contract.functions.symbol().call()}")
 # Total Supply
-#print("Total Supply: " f"{web3.fromWei(totalSupply, 'ether')}")
+print("Total Supply: " f"{web3.fromWei(totalSupply, 'ether')}")
 
+############################################################################################################################################
+#################################################################  Transaction ##########################################################
+############################################################################################################################################
+# account = web3.toChecksumAddress('0x596FbF10a5129fa7ae38FD6135C0954d415ea7Bc')
+# account_2 = web3.toChecksumAddress('0x5144C21864E24235E81F7e8c4a8d60663666eA94')
 
-# Transactions!!!
+# # Get the nonce // prevents you of sending a tx twice
+# nonce = web3.eth.getTransactionCount(account)
 
+# # Build a transaction
+# tx = {
+#     'nonce': nonce,
+#     'to': account_2,
+#     'value': web3.toWei(0.01, 'ether'),
+#     'gas': 200000,
+#     'gasPrice': web3.toWei('2', 'gwei')
+# }
 
-account_1 = "0x902f8fc364E7a4572f0626eecB54Ff600aea180e"
-account_2 = "0x35892aDEA3965ED9B5f093B46823779fAC0294Ee"
-private_key = "705c3bfa922e65ca13db8638797e07cf90f66c51c08ff7772c7202c8fbea7c6a"
-
-# Get the nonce // prevents you of sending a tx twice
-nonce = web3.eth.getTransactionCount(account_1)
-
-# Build a transaction
-tx = {
-    'nonce': nonce,
-    'to': account_2,
-    'value': web3.toWei(1, 'ether'),
-    'gas': 2000000,
-    'gasPrice': web3.toWei('50', 'gwei')
-}
-
-# Sign transaction
-signed_tx = web3.eth.account.signTransaction(tx, private_key)
-# Send transaction
-tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-# Get transaction hash
-print(web3.toHex(tx_hash))
+# # Sign transaction
+# signed_tx = web3.eth.account.signTransaction(tx, privateKey)
+# # Send transaction
+# tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+# # Get transaction hash
+# print(web3.toHex(tx_hash))
